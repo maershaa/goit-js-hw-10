7f6f7a05bc6589e44acafc74a4e19003d455e26e
maershaa <valeria.work2022@gmail.com>
2023-09-14 11:44:00 +0300

    Update cat-api.js

--- src/cat-api.js
+++ src/cat-api.js
@@ -4,7 +4,8 @@ import { createMarkup } from './markup.js';
 // Установим заголовок для всех запросов с ключом доступа
 axios.defaults.headers.common['x-api-key'] =
   'live_m6bWAmbCUpZFpjwTSQIcwzhCw2H822bRJfscfRObrB30g8VI6R23vJzjXH2DXoxA';
-import { BASE_URL } from './index';
+
+const BASE_URL = 'https://api.thecatapi.com/v1/';
 
 const refs = {
   breedSelect: document.querySelector('.breed-select'),
